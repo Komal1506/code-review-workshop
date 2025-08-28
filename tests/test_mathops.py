a9abbaba6b0e3fa31638219c8815a42980e2e663
Komal1506 <130238954+Komal1506@users.noreply.github.com>
2025-08-28 15:55:34 +0530

    Update test_mathops.py

--- tests/test_mathops.py
+++ tests/test_mathops.py
@@ -1,8 +1,12 @@
 import sys
 import os
+
+# E402 fix: Keep all imports at the top
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from src.mathops import multiply
 
+
+# E302 fix: Add two blank lines before function
 def test_multiply_basic():
     assert multiply(4, 5) == 20
